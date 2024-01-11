95047bcf6b6e71c39c64e96f42e31120756b5a2b
Shahwaiz Islam <s1404islam@gmail.com>
2024-01-11 23:46:02 +0530

    Final Commit

--- src/WeatherApp/InfoBox.jsx
+++ src/WeatherApp/InfoBox.jsx
@@ -3,9 +3,15 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "./InfoBox.css";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 export default function InfoBox({info}){
     const INIT_URL="https://images.unsplash.com/photo-1680352267694-a7fd4c33d4e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
 
+    const HOT_URL="https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8SG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL="https://images.unsplash.com/photo-1615226120487-90ec48f5d316?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fENPTEQlMjBXRUFUSEVSfGVufDB8fDB8fHww";
+    const RAIN_URL="https://images.unsplash.com/photo-1564599826778-dcb8f1481c51?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1vbnNvb24lMjB3ZWF0aGVyfGVufDB8fDB8fHww";
     return (
        <div className="infoBox">
             {/*<h1>Weather Info -{info.weather}</h1>*/}
@@ -13,12 +19,12 @@ export default function InfoBox({info}){
            <Card sx={{ maxWidth: 345 }}>
                <CardMedia
                    sx={{ height: 140 }}
-                   image={INIT_URL}
+                   image={info.humidity>95?RAIN_URL:(info.temp>15)?HOT_URL:COLD_URL}
                        title="green iguana"
                />
                <CardContent>
                    <Typography gutterBottom variant="h5" component="div">
-                       {info.city}
+                       {info.city } {info.humidity>95?<ThunderstormIcon/>:(info.temp>15)?<WbSunnyIcon/>:<AcUnitIcon/>}
                    </Typography>
                    <Typography variant="body2" color="text.secondary" component={"span"}>
                        <p>Temperature = {info.temp}&deg;C</p>
